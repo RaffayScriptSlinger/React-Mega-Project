60f2502bb2f9fd9166bc7bde192f4eabadacf7d4
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2025-01-04 23:07:44 +0500

    Login && SignUp Pages Added!

--- src/components/index.js
+++ src/components/index.js
@@ -8,5 +8,7 @@ import PostCard from "./postCard/postCard"
 import Input from "./Input"
 import RITE from "./RITE"
 import Select from "./Select/Select"
+import SignUp from "./SginUp/SignUp"
+import Login from "./"
 
 export { Header, Footer, LogoutBtn, Logo, Container, Button, PostCard, Input, RITE, Select }
